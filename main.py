b8621409e39b1b3894f0cf39cdb19cfb9e515cd6
Vanshika Kumawat <165902840+VanshikaKumawat@users.noreply.github.com>
2025-06-10 19:20:40 +0530

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from src.data_prep import load_data, preprocess_data, save_processed_data
+
+df = load_data("data/raw/transaction_dataset.csv")
+df = preprocess_data(df)
+save_processed_data(df, "data/processed/cleaned_wallet_data.csv")
 # Main ML pipeline will go here
 
 if __name__ == "__main__":
